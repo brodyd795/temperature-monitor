0ffeaabb57decb76cc063f97d2d1854b5f185023
Brody Dingel <brodydingel795@gmail.com>
2020-07-11 18:08:46 -0500

    Use average of last 5 readings and update README

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Temperature Monitor
+# Puck.js Temperature Monitor
 
-A simple temperature checker using a Puck.js to check the temperature in a room and blink different colors depending on the temperature.
+A simple temperature checker using a Puck.js to check the temperature in a room and blink different colors depending on the temperature. My wife uses this to check the temperature in our baby's room at night to make sure it's an okay temperature for the baby.
+
+### Deploying the app
+
+- Purchase a Puck.js and follow the set-up instructions [here](https://www.puck-js.com/)
+- Go to the [online IDE](https://www.espruino.com/ide/) and enter the JavaScript code
+- Start the app by sending the code via Bluetooth to the Puck.js
+- Press the Puck's built-in button, and watch it work!
+
+### Interpretation of colors
+- Green: okay temperature
+- Red: too hot
+- Blue: too cold
--- temp-monitor/temp-monitor.js
+++ temp-monitor/temp-monitor.js
@@ -1,11 +1,12 @@
-let temp, message;
+let temp, average;
 let on = false;
+let lastFiveTemps = [];
 
 const print = () => {};
 
-const blinkDuration = 200;
-const tooHot = 77;
-const tooCold = 76;
+const blinkDuration = 1000;
+const tooHot = 72;
+const tooCold = 65;
 
 function blink(LED) {
   LED.write(false);
@@ -17,14 +18,22 @@ function blink(LED) {
 
 function checkTemp() {
   temp = E.getTemperature() * 9/5 + 32;
-  if (temp >= tooHot) {
+  if (lastFiveTemps.length === 5) {
+    lastFiveTemps.shift();
+  }
+  lastFiveTemps.push(temp);
+  
+  average = lastFiveTemps.reduce((a, b) => a + b) / lastFiveTemps.length;
+  
+  if (average >= tooHot) {
     blink(LED1);
-  } else if (temp <= tooCold) {
+  } else if (average <= tooCold) {
     blink(LED3);
   } else {
     blink(LED2);
   }
-  print('Temp is ' + temp);
+  print(lastFiveTemps);
+  print('Temp average is ' + average);
 }
 
 setInterval(() => {
